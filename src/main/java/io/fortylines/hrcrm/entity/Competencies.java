805f93b3a10d4810cec1a43f91e64fe2ecf8aaf3
ectomorph312 <ectomorph312@gmail.com>
2021-03-24 13:11:39 +0600

    Revert "modified crud"
    This reverts commit f6030b02184d2ffbf1b53e35049ca57f7cdef1ad.

--- src/main/java/io/fortylines/hrcrm/entity/Competencies.java
+++
@@ -1,6 +0,0 @@
-package io.fortylines.hrcrm.entity;
-
-public enum Competencies {
-
-    Java, Python, JavaScript, PHP;
-}
--- src/main/java/io/fortylines/hrcrm/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package io.fortylines.hrcrm.repository;
-
-import io.fortylines.hrcrm.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByFirstName(String firstName);
-}
